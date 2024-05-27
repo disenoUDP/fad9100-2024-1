12667b2749309bf0d9f4f3bd0544dabb3af27bbb
aarón montoya-moraga <montoyamoraga@gmail.com>
2024-05-26 21:19:29 -0400

    corregir error mongoose

--- bitacoras/Pluumsy/encargo-03/src/index.js
+++ bitacoras/Pluumsy/encargo-03/src/index.js
@@ -2,24 +2,24 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const multer = require('multer');
-const {v4: uuidv4} = require ('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 // inicializaciones
 const app = express();
 
 // settings
 app.set('port', process.env.PORT || 3000);
-app.set('views' , path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // middlewares
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 const storage = multer.diskStorage({
-    destination: path.join(__dirname, 'public/img/uploads'),
-    filename: (req, file, cb, filename) => {
-        cb(null, uuidv4() + path.extname(file.originalname));
-    }
+  destination: path.join(__dirname, 'public/img/uploads'),
+  filename: (req, file, cb, filename) => {
+    cb(null, uuidv4() + path.extname(file.originalname));
+  },
 });
 app.use(multer({ storage: storage }).single('image'));
 
@@ -28,9 +28,9 @@ app.use(multer({ storage: storage }).single('image'));
 // routes
 app.use(require('./routes/index'));
 
-// static files 
+// static files
 
-// start the server 
+// start the server
 app.listen(app.get('port'), () => {
-console.log(`server on port ${app.get('port')}`);
+  console.log(`server on port ${app.get('port')}`);
 });
--- bitacoras/Pluumsy/encargo-03/src/models/image.js
+++ bitacoras/Pluumsy/encargo-03/src/models/image.js
@@ -1,13 +1,13 @@
-const { Schema, model } = require('moongoose');
+const { Schema, model } = require('mongoose');
 
 const imageSchema = new Schema({
- title: { type: String },
- description: { type: String },
- filename: { type: String },
- originalname: { type: String },
- nimetype: { type: String },
- size: { type: Number },
- created_at: { type: Date, default: Date.now() }   
+  title: { type: String },
+  description: { type: String },
+  filename: { type: String },
+  originalname: { type: String },
+  nimetype: { type: String },
+  size: { type: Number },
+  created_at: { type: Date, default: Date.now() },
 });
 
 module.exports = model('image', imageSchema);
